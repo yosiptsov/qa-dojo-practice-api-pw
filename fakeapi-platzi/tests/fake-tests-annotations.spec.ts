38399c6857d867e7cc5dd250aec43d91320ff78f
Yurii Osiptsov <yosiptsov@gmail.com>
2026-06-29 19:00:45 +0300

    refactor test annotations for clarity; ensure proper usage of skip and fixme methods

--- fakeapi-platzi/tests/fake-tests-annotations.spec.ts
+++ fakeapi-platzi/tests/fake-tests-annotations.spec.ts
@@ -2,17 +2,15 @@ import { test, expect } from "@playwright/test";
 import { TAG } from "./tags";
 
 test.describe("Fake tests to see how test annotations work", { tag: TAG.annotations }, () => {
-  test("|test id: L12:t0| - This test should be skipped", async ({}) => {
-    test.skip();
+  test.skip("|test id: L12:t0| - This test should be skipped", async ({}) => {
     expect(3).toBe(3);
   });
 
-  test("|test id: L12:t1| - This test should be skipped - waiting for fix", async ({}) => {
-    test.fixme();
+  test.fixme("|test id: L12:t1| - This test should be skipped - waiting for fix", async ({}) => {
     expect(3).toBe(3);
   });
 
-  test("|test id: L12:t2| - This test should allow failure", async ({}) => {
+  test.fail("|test id: L12:t2| - This test should allow failure", async ({}) => {
     test.fail();
     console.log("This test should allow failure");
     expect(2).toBe(3);
