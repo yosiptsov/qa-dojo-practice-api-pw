9fd3eb9f15c3e57025f5f5680a38dac40c3bca77
Yurii Osiptsov <yosiptsov@gmail.com>
2026-07-04 20:57:46 +0300

    refactor: enhance user retrieval tests; update assertions and improve role validation for users

--- tests/fakeapi-platzi/users/users.spec.ts
+++ tests/fakeapi-platzi/users/users.spec.ts
@@ -51,14 +51,14 @@ test.describe("Verification of endpoint /api/v1/users", { tag: TAG.users }, () =
       });
     });
 
-    test("GET - list of all users should: |test id: L13-3:t2|", async ({ request, requestData }) => {
+    test("GET - response for all users should have: |test id: L13-3:t2|", async ({ request, requestData }) => {
       //Arrange
       const createdUser = await test.step("Create a new user", async () => {
         const createdUser = await createUser(request, requestData.newUser);
         return createdUser;
       });
       //Act
-      const allUsersList = await test.step("get list of all users", async () => {
+      const allUsersList = await test.step("Get list of all users", async () => {
         const allUsersList = await readUsers(request);
         return allUsersList;
       });
@@ -74,7 +74,7 @@ test.describe("Verification of endpoint /api/v1/users", { tag: TAG.users }, () =
       });
 
       await test.step("correct headers", () => {
-        const headers = createdUser.response.headers();
+        const headers = allUsersList.response.headers();
         expect.soft(headers["content-type"]).toContain("application/json");
         expect.soft(headers["cross-origin-opener-policy"]).toBe("same-origin");
         expect.soft(headers["cross-origin-resource-policy"]).toBe("same-origin");
@@ -84,10 +84,55 @@ test.describe("Verification of endpoint /api/v1/users", { tag: TAG.users }, () =
       await test.step("contains just added user", async () => {
         const foundUser = allUsersList.json.find((elem) => elem.id === createdUser.json.id);
         expect(foundUser).toBeDefined();
-        console.log(foundUser);
       });
 
-      await test.step("all users have role defined, one of customer | admin", async () => {});
+      await test.step("all users have role defined, one of customer | admin", async () => {
+        const allUsersArray = allUsersList.json;
+        for (const user of allUsersArray) {
+          expect(["customer", "admin"]).toContain(user.role);
+        }
+      });
+    });
+
+    test("GET - for one user by ID response should: |test id: L13-3:t3|", async ({ request, requestData }) => {
+      //Arrange
+      const createdUser = await test.step("Create a new user", async () => {
+        const createdUser = await createUser(request, requestData.newUser);
+        return createdUser;
+      });
+      //Act
+      const getCreatedUser = await test.step("Get created user", async () => {
+        const getCreatedUser = await readUsers(request, createdUser.json.id);
+        return getCreatedUser;
+      });
+      //Assert
+      await test.step("response status is 201 / OK", () => {
+        expect(getCreatedUser.response.status()).toBe(200);
+        expect(getCreatedUser.response.statusText()).toBe("OK");
+      });
+
+      await test.step("JSON schema matches ZOD template", async () => {
+        const data = UserResponseSchema.safeParse(getCreatedUser.json);
+        expect(data.success, { message: data.error?.message }).toBeTruthy();
+      });
+
+      await test.step("correct headers", () => {
+        const headers = getCreatedUser.response.headers();
+        expect.soft(headers["content-type"]).toContain("application/json");
+        expect.soft(headers["cross-origin-opener-policy"]).toBe("same-origin");
+        expect.soft(headers["cross-origin-resource-policy"]).toBe("same-origin");
+        expect.soft(headers["content-security-policy"]).toContain("base-uri");
+      });
+
+      await test.step("all fields of gotten user equal to created", () => {
+        expect(getCreatedUser.json).toMatchObject({
+          id: createdUser.json.id,
+          name: requestData.newUser.name,
+          password: requestData.newUser.password,
+          email: requestData.newUser.email,
+          avatar: requestData.newUser.avatar,
+        });
+      });
     });
   });
 });
