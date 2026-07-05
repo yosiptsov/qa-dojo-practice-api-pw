7caa247005a021747618bfe0c02a6d3b67581bbd
Yurii Osiptsov <yosiptsov@gmail.com>
2026-07-05 20:15:28 +0300

    refactor: update auth test to fetch news using fixture; enhance request handling with API key

--- tests/newsapi/auth.spec.ts
+++ tests/newsapi/auth.spec.ts
@@ -1,5 +1,16 @@
 import { test } from "@playwright/test";
 
-test("auth", async () => {
-  console.log("something");
+test("Get some news, auth trough the fixture", async ({ request }) => {
+  const url = "/v2/everything";
+  const apiKey = process.env.NEWSAPI_API_KEY;
+  const response = await request.get(url, {
+    failOnStatusCode: true,
+    params: {
+      apiKey: `${apiKey}`,
+      q: "Apple",
+      from: "2026-07-01",
+    },
+  });
+  const json = await response.json();
+  console.log(json);
 });
--- tests/newsapi/fixtures.ts
+++ tests/newsapi/fixtures.ts
@@ -2,4 +2,18 @@ import { APIRequestContext, test as base, request as APIRequest } from "@playwri
 
 type Fixtures = {};
 
-export const test = base.extend({});
+export const test = base.extend<Fixtures>({
+  // login
+  request: async ({}, use) => {
+    const apiKey = process.env.NEWSAPI_API_KEY;
+
+    const req = await APIRequest.newContext({
+      failOnStatusCode: true,
+      params: {
+        apiKey: `${apiKey}`,
+      },
+    });
+
+    await use(req);
+  },
+});
